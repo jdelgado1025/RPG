2dbc862e44f12b63aa4bc2e783ebc3def7721726
Jose Delgado <jdelgado1025@gmail.com>
2022-09-29 21:47:55 -0700

    Bare bones DBContext class

---
+++ Data/RpgContext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RPG.Data
+{
+    public class RpgContext : DbContext
+    {
+        
+    }
+}
