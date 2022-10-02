0c6d5f0dd69b6213094621b1ce62ae1cd1082e45
Jose Delgado <jdelgado1025@gmail.com>
2022-10-02 15:36:51 -0700

    Updated RpgClass to public

--- DataTransferObjects/Character/AddCharacterDTO.cs
+++ DataTransferObjects/Character/AddCharacterDTO.cs
@@ -12,6 +12,6 @@ namespace RPG.DataTransferObjects.Character
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
-        RpgClass Class {get; set;} = RpgClass.Knight;
+        public RpgClass Class {get; set;} = RpgClass.Knight;
     }
 }
