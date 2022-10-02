9d5b90fa5aaef6cc7eca5a042dcf9a8f3621b96f
Jose Delgado <jdelgado1025@gmail.com>
2022-10-02 15:37:13 -0700

    Updated implementations to SqlServer

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -5,7 +5,6 @@ namespace RPG.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
-        private static List<Character> characters = new List<Character>();
         private readonly IMapper _mapper;
         private readonly RpgContext _context;
 
@@ -16,60 +15,82 @@ namespace RPG.Services.CharacterService
         }
         public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
         {
-            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+            var response = new ServiceResponse<List<GetCharacterDTO>>();
             Character character = _mapper.Map<Character>(newCharacter);
-            if(characters.Count == 0){
-                character.Id = 0;
-            }else{
-                character.Id = characters.Max(c => c.Id) + 1;
+            
+            try
+            {
+                _context.Characters.Add(character);
+                await _context.SaveChangesAsync();
+                response.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync(); //_mapper.Map<List<GetCharacterDTO>>(characters);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
             }
             
-            characters.Add(character);
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList(); //_mapper.Map<List<GetCharacterDTO>>(characters);
-            return serviceResponse;
+            return response;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacter(int id)
         {
-            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+            var response = new ServiceResponse<List<GetCharacterDTO>>();
+
+            var character = await _context.Characters.FindAsync(id);
+
+            if(character == null)
+            {
+                response.Success = false;
+                response.Message = $"Character with id {id} does not exist";
+                return response;
+            }
 
             try
             {
-                var character = characters.First(c => c.Id == id);
-                characters.Remove(character);
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+                _context.Characters.Remove(character);
+                await _context.SaveChangesAsync();
+                response.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
             }
             catch (Exception ex)
             {
-                serviceResponse.Success = false;
-                serviceResponse.Message = ex.Message;
+                response.Success = false;
+                response.Message = ex.Message;
             }
 
-            return serviceResponse;
+            return response;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAll()
         {
             var response = new ServiceResponse<List<GetCharacterDTO>>();
-            var dbCharacters = await _context.Characters.ToListAsync();
-            response.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+            var characters = await _context.Characters.ToListAsync();
+            response.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
             return response; //_mapper.Map<List<GetCharacterDTO>>(characters);
         }
 
         public async Task<ServiceResponse<GetCharacterDTO>> GetCharacter(int id)
         {
-            var serviceResponse = new ServiceResponse<GetCharacterDTO>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
-            return serviceResponse;
+            var response = new ServiceResponse<GetCharacterDTO>();
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);//characters.FirstOrDefault(c => c.Id == id);
+            response.Data = _mapper.Map<GetCharacterDTO>(character);
+
+            if(character == null)
+            {
+                response.Success = false;
+                response.Message = $"The character with id {id} does not exist.";
+            }
+            
+            return response;
         }
 
         public async Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updatedCharacter)
         {
-            var serviceResponse = new ServiceResponse<GetCharacterDTO>();
+            var response = new ServiceResponse<GetCharacterDTO>();
+
             try
             {
-                var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+                var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
                 character.Class = updatedCharacter.Class;
                 character.Defense = updatedCharacter.Defense;
                 character.HitPoints = updatedCharacter.HitPoints;
@@ -77,15 +98,17 @@ namespace RPG.Services.CharacterService
                 character.Name = updatedCharacter.Name;
                 character.Strength = updatedCharacter.Strength;
 
-                serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
+                // _context.Entry(character).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                response.Data = _mapper.Map<GetCharacterDTO>(character);
             }
             catch (Exception ex)
             {
-                serviceResponse.Success = false;
-                serviceResponse.Message = ex.Message;
+                response.Success = false;
+                response.Message = ex.Message;
             }
 
-            return serviceResponse;
+            return response;
         }
     }
 }
